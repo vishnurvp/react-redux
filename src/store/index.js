7bfdd3502e4cc513982d1a85cbae5accce842a21
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-17 15:23:37 +0530

    Task_12

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") return { counter: state.counter + 1 };
+  if (action.type === "decrement") return { counter: state.counter - 1 };
+  if (action.type === "incrementBy2") return { counter: state.counter + 2 };
+  if (action.type === "decrementBy2") return { counter: state.counter - 2 };
+  
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
